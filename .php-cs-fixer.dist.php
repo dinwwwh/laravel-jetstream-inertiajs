13eab5c65932f0e2640c7b8b65f3aaf24ba4aee3
dinhdjj <dinhdjj@gmail.com>
2021-12-26 15:45:04 +0700

    chore(lint): config `eslint` `prettier` `php-cs-fixer`, `commitlint`, `lint-staged`

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,40 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$rules = [
+    '@PHPUnit84Migration:risky' => true,
+    '@PHP80Migration:risky' => true,
+    '@PHP81Migration' => true,
+
+    '@PhpCsFixer' => true,
+    '@PhpCsFixer:risky' => true,
+
+    '@Symfony' => true, // To override the @PhpCsFixer rules
+    '@Symfony:risky' => true, // To override the @PhpCsFixer:risky rules
+
+    /* 50 - 50 : consider */
+    'php_unit_internal_class' => false,
+    'php_unit_test_class_requires_covers' => false,
+
+    'declare_strict_types' => false,
+];
+
+$finder = Finder::create()
+    ->in([
+        __DIR__,
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true)
+;
+
+$config = new Config();
+
+return $config->setFinder($finder)
+    ->setRules($rules)
+    ->setRiskyAllowed(true)
+    ->setUsingCache(true)
+;
