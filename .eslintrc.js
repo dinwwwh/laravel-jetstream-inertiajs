325b07983bf924c5c65c1b465bd411d2e682fce4
dinhdjj <dinhdjj@gmail.com>
2021-12-26 16:43:02 +0700

    chore(lint): make eslint more compatible for vue3

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['plugin:vue/essential', 'airbnb-base', 'prettier'],
+    extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'prettier'],
     parserOptions: {
         ecmaVersion: 13,
         parser: '@typescript-eslint/parser',
         sourceType: 'module',
     },
     plugins: ['vue', '@typescript-eslint'],
-    rules: {},
+    rules: {
+        'vue/multi-word-component-names': 'off',
+    },
 };
