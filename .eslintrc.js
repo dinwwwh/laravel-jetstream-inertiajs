1f1ca8eaab6fca2d8e8c9552dfea58dabf8016a3
dinhdjj <dinhdjj@gmail.com>
2021-12-28 13:12:44 +0700

    chore(eslint): use `tailwindcss` plugin
    used to find issue with order tailwindcss classnames, helper for migrate tailwindcss 2 to 3...

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'prettier'],
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'airbnb-base',
+        'prettier',
+        'plugin:tailwindcss/recommended',
+    ],
     parserOptions: {
         ecmaVersion: 13,
         parser: '@typescript-eslint/parser',
         sourceType: 'module',
     },
-    plugins: ['vue', '@typescript-eslint'],
+    plugins: ['vue', '@typescript-eslint', 'tailwindcss'],
     rules: {},
     settings: {
         'import/resolver': {
