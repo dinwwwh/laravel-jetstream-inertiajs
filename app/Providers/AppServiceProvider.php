3f3c7c8442375074432831b30f042bb7dcc805ab
dinhdjj <dinhdjj@gmail.com>
2022-01-15 09:08:18 +0700

    chore(package): make `barryvdh/laravel-ide-helper` is autoload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
     }
 
     /**
