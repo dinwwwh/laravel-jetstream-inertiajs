f2cf9e9be47432edf8c9ed7b899820abe79fbe4e
dinhdjj <dinhdjj@gmail.com>
2022-05-11 20:53:44 +0700

    chore(linting): uses `larastan`

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,13 +9,14 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param \Illuminate\Http\Request $request
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): string|null
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
             return route('login');
         }
+
+        return null;
     }
 }
